d6b2ecb4874de8d0792f636bd4b190efa1c59ec1
KanteshMurade <97043824+KanteshMurade@users.noreply.github.com>
2026-04-26 23:43:24 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_payout, payout_list, get_balance_view
+from .views import create_payout, payout_list, get_balance_view, add_credit
 
 urlpatterns = [
     path('payouts', create_payout),
     path('payouts/list/', payout_list),
     path('balance/<int:merchant_id>', get_balance_view),
+    path('credit', add_credit),   
 ]
