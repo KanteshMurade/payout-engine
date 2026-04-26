ea5433b4d0e2802c95c5a7eef3cd0744ddc5cf77
KanteshMurade <kanteshmurade@gmail.com>
2026-04-26 21:58:48 +0530

    fix import error remove LedgerEntry

--- core/apps.py
+++ core/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
 
+
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-
-    def ready(self):
-        from .models import Merchant, LedgerEntry
-
-        if not Merchant.objects.exists():
-            m = Merchant.objects.create(name="Auto Merchant")
-            LedgerEntry.objects.create(
-                merchant=m,
-                amount_paise=10000,
-                type='credit'
-            )
