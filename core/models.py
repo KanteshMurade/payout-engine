9b33641ce2516149c8a7f4adf06917b64936bca4
KanteshMurade <kanteshmurade@gmail.com>
2026-04-27 01:03:19 +0530

    final working payout system with worker

--- core/models.py
+++ core/models.py
@@ -1,40 +1,18 @@
 from django.db import models
 
 class Merchant(models.Model):
-    name = models.CharField(max_length=100)
-
+    name = models.CharField(max_length=255)
 
 class Payout(models.Model):
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('processing', 'Processing'),
-        ('completed', 'Completed'),
-        ('failed', 'Failed'),
-    ]
-
     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
-    amount = models.BigIntegerField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-    attempts = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    amount = models.IntegerField()
+    status = models.CharField(max_length=20, default="pending")
 
 class LedgerEntry(models.Model):
-    TYPE_CHOICES = [
-        ('credit', 'Credit'),
-        ('debit', 'Debit'),
-    ]
-
     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
-    amount = models.BigIntegerField()
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    amount = models.IntegerField()
+    type = models.CharField(max_length=10)
 
 class IdempotencyKey(models.Model):
     key = models.CharField(max_length=255)
-    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
-    payout = models.OneToOneField(Payout, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('key', 'merchant')
+    payout = models.ForeignKey(Payout, on_delete=models.CASCADE)
--- payout_engine/settings.py
+++ payout_engine/settings.py
@@ -17,9 +17,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'rest_framework',
     'corsheaders',  
-    'core',
+    'core.apps.CoreConfig',
 ]
 
 # MIDDLEWARE
